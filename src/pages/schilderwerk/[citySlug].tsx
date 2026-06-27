e69f5cfe4e6984dce028d5cbe23552c88a2b5550
projects2026dmr <projects2026.dmr@gmail.com>
2026-06-27 18:14:28 +0200

    Update [citySlug].tsx

--- src/pages/schilderwerk/[citySlug].tsx
+++ src/pages/schilderwerk/[citySlug].tsx
@@ -1,44 +1,54 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { LOCATIONS } from "@/data/locations";
+
 import SEOHead from "@/components/SEOHead";
 import ServiceLocationTemplate from "@/templates/ServiceLocationTemplate";
+
 import Content from "@/components/serviceBlocks/schilderwerk/Content";
 import FAQ from "@/components/serviceBlocks/schilderwerk/FAQ";
 import CTA from "@/components/serviceBlocks/schilderwerk/CTA";
-import Breadcrumbs from "@/components/serviceBlocks/schilderwerk/Breadcrumbs";
 import Schema from "@/components/serviceBlocks/schilderwerk/Schema";
+
+import Breadcrumbs from "@/components/Breadcrumbs";
 
 const Page = () => {
   const { citySlug } = useParams();
-  const location = LOCATIONS.find(loc => loc.slug === citySlug);
+  const location = LOCATIONS.find((loc) => loc.slug === citySlug);
 
   if (!location) {
     return <div>Not Found</div>;
   }
 
   const serviceName = "Schilderwerk";
+  const serviceSlug = "schilderwerk";
   const city = location.city;
 
-  const metaTitle = `${serviceName} in ${city}`;
-  const metaDescription = `NexStuc verzorgt professioneel ${serviceName.toLowerCase()} in ${city} en omgeving.`;
+  const metaTitle = `Professioneel ${serviceName.toLowerCase()} in ${city} | NexStuc schilder specialist`;
+  const metaDescription = `Vakkundig ${serviceName.toLowerCase()} in ${city} door NexStuc. Strakke afwerking, duurzame verf en garantie. Vraag een vrijblijvende offerte aan in ${city}.`;
 
   return (
     <>
       <SEOHead
         title={metaTitle}
         description={metaDescription}
-        canonical={`https://www.nexstuc.nl/schilderwerk/${location.slug}`}
+        canonical={`https://www.nexstuc.nl/${serviceSlug}/${location.slug}`}
       />
 
       <ServiceLocationTemplate
         serviceName={serviceName}
         city={city}
+        breadcrumbs={
+          <Breadcrumbs
+            serviceSlug={serviceSlug}
+            serviceName={serviceName}
+            city={city}
+          />
+        }
         content={<Content serviceName={serviceName} city={city} />}
         faq={<FAQ serviceName={serviceName} city={city} />}
         cta={<CTA serviceName={serviceName} city={city} />}
-        breadcrumbs={<Breadcrumbs serviceName={serviceName} city={city} slug="schilderwerk" />}
-        schema={<Schema serviceName={serviceName} city={city} slug="schilderwerk" />}
+        schema={<Schema serviceName={serviceName} city={city} />}
       />
     </>
   );
