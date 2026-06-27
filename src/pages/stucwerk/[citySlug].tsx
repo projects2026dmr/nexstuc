aef0a6d36c7954ce79bb6accef3b2aa80a054ad0
projects2026dmr <projects2026.dmr@gmail.com>
2026-06-27 17:35:08 +0200

    Update [citySlug].tsx

--- src/pages/stucwerk/[citySlug].tsx
+++ src/pages/stucwerk/[citySlug].tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { LOCATIONS } from "@/data/locations";
+
 import SEOHead from "@/components/SEOHead";
 import ServiceLocationTemplate from "@/templates/ServiceLocationTemplate";
+
 import Content from "@/components/serviceBlocks/stucwerk/Content";
 import FAQ from "@/components/serviceBlocks/stucwerk/FAQ";
 import CTA from "@/components/serviceBlocks/stucwerk/CTA";
-import Breadcrumbs from "@/components/serviceBlocks/stucwerk/Breadcrumbs";
 import Schema from "@/components/serviceBlocks/stucwerk/Schema";
+
+import Breadcrumbs from "@/components/Breadcrumbs";
 
 const Page = () => {
   const { citySlug } = useParams();
-  const location = LOCATIONS.find(loc => loc.slug === citySlug);
+  const location = LOCATIONS.find((loc) => loc.slug === citySlug);
 
   if (!location) {
     return <div>Not Found</div>;
   }
 
   const serviceName = "Stucwerk";
+  const serviceSlug = "stucwerk";
   const city = location.city;
 
   const metaTitle = `${serviceName} in ${city}`;
@@ -28,17 +32,23 @@ const Page = () => {
       <SEOHead
         title={metaTitle}
         description={metaDescription}
-        canonical={`https://www.nexstuc.nl/stucwerk/${location.slug}`}
+        canonical={`https://www.nexstuc.nl/${serviceSlug}/${location.slug}`}
       />
 
       <ServiceLocationTemplate
         serviceName={serviceName}
         city={city}
+        breadcrumbs={
+          <Breadcrumbs
+            serviceSlug={serviceSlug}
+            serviceName={serviceName}
+            city={city}
+          />
+        }
         content={<Content serviceName={serviceName} city={city} />}
         faq={<FAQ serviceName={serviceName} city={city} />}
         cta={<CTA serviceName={serviceName} city={city} />}
-        breadcrumbs={<Breadcrumbs serviceName={serviceName} city={city} slug="stucwerk" />}
-        schema={<Schema serviceName={serviceName} city={city} slug="stucwerk" />}
+        schema={<Schema serviceName={serviceName} city={city} />}
       />
     </>
   );
