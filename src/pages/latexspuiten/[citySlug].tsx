a7ce8d26093d7e524d25e63ec669340a9165b717
projects2026dmr <projects2026.dmr@gmail.com>
2026-06-27 18:12:56 +0200

    Update [citySlug].tsx

--- src/pages/latexspuiten/[citySlug].tsx
+++ src/pages/latexspuiten/[citySlug].tsx
@@ -1,44 +1,54 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { LOCATIONS } from "@/data/locations";
+
 import SEOHead from "@/components/SEOHead";
 import ServiceLocationTemplate from "@/templates/ServiceLocationTemplate";
+
 import Content from "@/components/serviceBlocks/latexspuiten/Content";
 import FAQ from "@/components/serviceBlocks/latexspuiten/FAQ";
 import CTA from "@/components/serviceBlocks/latexspuiten/CTA";
-import Breadcrumbs from "@/components/serviceBlocks/latexspuiten/Breadcrumbs";
 import Schema from "@/components/serviceBlocks/latexspuiten/Schema";
+
+import Breadcrumbs from "@/components/Breadcrumbs";
 
 const Page = () => {
   const { citySlug } = useParams();
-  const location = LOCATIONS.find(loc => loc.slug === citySlug);
+  const location = LOCATIONS.find((loc) => loc.slug === citySlug);
 
   if (!location) {
     return <div>Not Found</div>;
   }
 
   const serviceName = "Latexspuiten";
+  const serviceSlug = "latexspuiten";
   const city = location.city;
 
-  const metaTitle = `${serviceName} in ${city}`;
-  const metaDescription = `NexStuc verzorgt professioneel ${serviceName.toLowerCase()} in ${city} en omgeving.`;
+  const metaTitle = `Strak ${serviceName.toLowerCase()} in ${city} | NexStuc latexspuit specialist`;
+  const metaDescription = `Professioneel ${serviceName.toLowerCase()} in ${city} door NexStuc. Strakke egale wanden, snelle oplevering en garantie. Vraag een vrijblijvende offerte aan in ${city}.`;
 
   return (
     <>
       <SEOHead
         title={metaTitle}
         description={metaDescription}
-        canonical={`https://www.nexstuc.nl/latexspuiten/${location.slug}`}
+        canonical={`https://www.nexstuc.nl/${serviceSlug}/${location.slug}`}
       />
 
       <ServiceLocationTemplate
         serviceName={serviceName}
         city={city}
+        breadcrumbs={
+          <Breadcrumbs
+            serviceSlug={serviceSlug}
+            serviceName={serviceName}
+            city={city}
+          />
+        }
         content={<Content serviceName={serviceName} city={city} />}
         faq={<FAQ serviceName={serviceName} city={city} />}
         cta={<CTA serviceName={serviceName} city={city} />}
-        breadcrumbs={<Breadcrumbs serviceName={serviceName} city={city} slug="latexspuiten" />}
-        schema={<Schema serviceName={serviceName} city={city} slug="latexspuiten" />}
+        schema={<Schema serviceName={serviceName} city={city} />}
       />
     </>
   );
