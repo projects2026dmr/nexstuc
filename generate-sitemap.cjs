887dc4a65d8d8573eb00b5ffeefe29c6125938f1
projects2026dmr <projects2026.dmr@gmail.com>
2026-06-28 16:26:22 +0200

    Update generate-sitemap.cjs

--- generate-sitemap.cjs
+++ generate-sitemap.cjs
@@ -1,1 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+
+const { LOCATIONS } = require("./src/data/locations");
+const { SERVICES } = require("./src/data/services");
+
+const baseUrl = "https://nexstuc.vercel.app";
+
+const staticUrls = [
+  "",
+  "/stukadoor",
+  "/stucwerk",
+  "/latexspuiten",
+  "/spackspuiten",
+  "/schilderwerk",
+  "/beton-cire",
+  "/badkamerrenovatie",
+];
+
+const locationUrls = [];
+
+SERVICES.forEach((service) => {
+  LOCATIONS.forEach((loc) => {
+    locationUrls.push(`/${service.slug}/${loc.slug}`);
+  });
+});
+
+const allUrls = [...staticUrls, ...locationUrls];
+
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allUrls
+  .map(
+    (url) => `
+  <url>
+    <loc>${baseUrl}${url}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>${url === "" ? "1.0" : "0.8"}</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+fs.writeFileSync(path.join(__dirname, "public", "sitemap.xml"), xml);
+
+console.log("Sitemap generated successfully!");
 
